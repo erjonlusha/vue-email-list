36e7681d45de8d0eea10c67ee3795bda76bdf157
Erjon <erjonlusha@gmail.com>
2021-01-12 02:45:17 +0100

    Genera 10 mail tramite api e stampale in una lista
    Usate Vue e Axios.
    La richiesta AJAX per gli indirizzi email inviatela al seguente link:
    https://flynn.boolean.careers/exercises/api/random/mail
    La lista di email va stampata a schermo nella pagina html.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let app = new Vue ({
+    el: "#app",
+    data : {
+        emails: []
+    },
+    mounted () {
+        for (let index = 0; index < 10; index++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(response => {
+              //console.log(response);       
+              let emailsCasuali = response.data.response;
+              this.emails.push(emailsCasuali);
+          })
+        }
+      }
+});
